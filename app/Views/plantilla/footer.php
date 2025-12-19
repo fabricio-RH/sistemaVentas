7e81781e9611e1c3ea01ca6045279695f98802c9
fabricio-RH <fabricioroque369@gmail.com>
2025-12-18 23:08:39 -0500

    Pruebasde cambio

--- app/Views/plantilla/footer.php
+++ app/Views/plantilla/footer.php
@@ -1,3 +1,11 @@
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Sistema creado por Fabricio RH &copy; 2025</div>
+        </div>
+    </div>
+</footer>
+
 </div> <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
